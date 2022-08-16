1e451271d72975671d36ea1a47b1ed7d057b31f4
Niki Balajadia <niki@Nikis-MacBook-Pro.local>
2022-08-15 17:32:50 -1000

    clear button done

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,20 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_NUMBER = "CLEAR_NUMBER"
 
 export const addOne = () => {
-    return({type:ADD_ONE});
+    return ({ type: ADD_ONE });
 }
 
 export const applyNumber = (number) => {
-    return({type:APPLY_NUMBER, payload:number});
+    return ({ type: APPLY_NUMBER, payload: number });
+}
+
+export const changeOperation = (operator) => {
+    return ({ type: CHANGE_OPERATION, payload: operator })
+}
+
+export const clearNumber = () => {
+    return ({ type: CLEAR_NUMBER });
 }
--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,23 @@ import './App.css';
 
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
+import { applyNumber, changeOperation, clearNumber } from '../actions'
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  const handleClick = (number) => {
+    dispatch(applyNumber(number))
+  }
+
+  const handleOperationClick = (operator) => {
+    dispatch(changeOperation(operator))
+  }
+
+  const handleClear = () => {
+    dispatch(clearNumber())
+  }
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -18,10 +32,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
 
-            <TotalDisplay value={0} />
+            <TotalDisplay value={state.total} />
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation} </span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
 
             <div className="row">
@@ -31,31 +45,31 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} />
-              <CalcButton value={2} />
-              <CalcButton value={3} />
+              <CalcButton value={1} onClick={() => handleClick(1)} />
+              <CalcButton value={2} onClick={() => handleClick(2)} />
+              <CalcButton value={3} onClick={() => handleClick(3)} />
             </div>
 
             <div className="row">
-              <CalcButton value={4} />
-              <CalcButton value={5} />
-              <CalcButton value={6} />
+              <CalcButton value={4} onClick={() => handleClick(4)} />
+              <CalcButton value={5} onClick={() => handleClick(5)} />
+              <CalcButton value={6} onClick={() => handleClick(6)} />
             </div>
 
             <div className="row">
-              <CalcButton value={7} />
-              <CalcButton value={8} />
-              <CalcButton value={9} />
+              <CalcButton value={7} onClick={() => handleClick(7)} />
+              <CalcButton value={8} onClick={() => handleClick(8)} />
+              <CalcButton value={9} onClick={() => handleClick(9)} />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"} />
-              <CalcButton value={"*"} />
-              <CalcButton value={"-"} />
+              <CalcButton value={"+"} onClick={() => handleOperationClick('+')} />
+              <CalcButton value={"*"} onClick={() => handleOperationClick('*')} />
+              <CalcButton value={"-"} onClick={() => handleOperationClick('-')} />
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"} />
+              <CalcButton value={"CE"} onClick={handleClear} />
             </div>
 
           </form>
